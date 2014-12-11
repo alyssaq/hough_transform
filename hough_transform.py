a8c7dcfb30c5540e75d5f841e4b0cb9215d21258
Alyssa <alyssaquek@gmail.com>
2014-12-11 21:04:23 +0800

    Comments and save output image

--- hough_transform.py
+++ hough_transform.py
@@ -2,6 +2,18 @@ import numpy as np
 from scipy import misc
 
 def hough_line(img):
+  """
+  Hough transform for lines
+
+  Input:
+  img - 2D binary image with nonzeros representing edges
+
+  Returns:
+  accumulator - 2D array of the hough transform accumulator
+  theta - array of angles used in computation, in radians.
+  rhos - array of rho values. Max size is 2 times the diagonal
+         distance of the input image.
+  """
   # theta = -90 to 89, rho = -diag_len to diag_len
   thetas = np.linspace(-np.pi/2, np.pi/2, 180)
   num_thetas = len(thetas)
@@ -10,8 +22,9 @@ def hough_line(img):
 
   width, height = img.shape
   diag_len = np.ceil(np.sqrt(width * width + height * height))
+  rhos = np.arange(-diag_len, diag_len)
 
-  # accumulator array of theta vs img
+  # accumulator array of theta vs rho
   accumulator = np.zeros((2 * diag_len, num_thetas), dtype=np.uint64)
   x_idxs, y_idxs = np.nonzero(img)
 
@@ -23,14 +36,20 @@ def hough_line(img):
       rho = round(x * cos_t[theta] + y * sin_t[theta]) + diag_len
       accumulator[rho, theta] += 1
 
-  return accumulator, thetas
+  return accumulator, thetas, rhos
 
 def show_hough_line(imgpath):
   import matplotlib.pyplot as plt
 
   img = misc.imread(imgpath)
-  accumulator, thetas = hough_line(img)
+  accumulator, thetas, rhos = hough_line(img)
+
+  plt.subplot(1, 2, 1)
+  plt.imshow(img, 'gray')
+  plt.subplot(1, 2, 2)
   plt.imshow(accumulator, cmap='jet')
+  plt.axis('off')
+  plt.savefig('imgs/output.png', bbox_inches='tight')
   plt.show()
 
 if __name__ == '__main__':
