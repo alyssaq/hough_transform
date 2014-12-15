2ce8b3c0375c99ac4a9e8f161a297ef5cbdb30ba
Alyssa <alyssaquek@gmail.com>
2014-12-16 01:41:01 +0800

    Rearranged and added comments in logical steps

--- hough_transform.py
+++ hough_transform.py
@@ -16,25 +16,28 @@ def hough_line(img, angle_step=1):
   rhos - array of rho values. Max size is 2 times the diagonal
          distance of the input image.
   """
-  # theta = -90 to 89, rho = -diag_len to diag_len
+  # Rho and Theta ranges
   thetas = np.deg2rad(np.arange(-90.0, 90.0, angle_step))
-  num_thetas = len(thetas)
-  cos_t = np.cos(thetas)
-  sin_t = np.sin(thetas)
-
   width, height = img.shape
   diag_len = np.ceil(np.sqrt(width * width + height * height))
   rhos = np.linspace(-diag_len, diag_len, diag_len * 2.0)
 
-  # accumulator array of theta vs rho
-  accumulator = np.zeros((2 * diag_len, num_thetas), dtype=np.uint64)
-  y_idxs, x_idxs = np.nonzero(img)
+  # Cache some resuable values
+  cos_t = np.cos(thetas)
+  sin_t = np.sin(thetas)
+  num_thetas = len(thetas)
 
+  # Hough accumulator array of theta vs rho
+  accumulator = np.zeros((2 * diag_len, num_thetas), dtype=np.uint64)
+  y_idxs, x_idxs = np.nonzero(img)  # (row, col) indexes to edges
+
+  # Vote in the hough accumulator
   for i in range(len(x_idxs)):
     x = x_idxs[i]
     y = y_idxs[i]
 
     for t_idx in range(num_thetas):
+      # Calculate rho. diag_len is added for a positive index
       rho = round(x * cos_t[t_idx] + y * sin_t[t_idx]) + diag_len
       accumulator[rho, t_idx] += 1
 
